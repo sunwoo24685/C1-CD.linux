ff4104985884f38124d51be40f0885f006fbec14
sunwoo <incheon.ac.kr>
2024-12-10 16:14:31 +0900

    dfsd

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-res.send("<h1>Incheon World!</h1>");
+    res.send("<h1>Incheon World!!!!!</h1>");
 });
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
